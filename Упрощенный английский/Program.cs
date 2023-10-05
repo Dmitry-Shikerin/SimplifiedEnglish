ea6409c73f76bab2053bc6ed55e684bc08639266
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-05 13:47:17 +0300

    Добавьте файлы проекта.

---
+++ Упрощенный английский/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Упрощенный_английский
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Search(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+    }
+}
